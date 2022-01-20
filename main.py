97cc3790f3373a650350c49a899e2b50285525b8
walterwhiteru <98005708+walterwhiteru@users.noreply.github.com>
2022-01-20 12:14:48 +0400

    drain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot
 from telebot import types
 from random import randint
-import requests
 
 bot = telebot.TeleBot('5014594116:AAHpVXrVwyRtrMJI93TEDBmHQCgtXWZdktA')
 
@@ -22,8 +21,5 @@ def text(message):
         bot.send_photo(chat_id=message.chat.id, caption='♿mineeer♿', photo='https://steamuserimages-a.akamaihd.net/ugc/31861554314034185/6605F9776017E5765DC3D9DE7E9D4887E652AC5B/?imw=512&amp;imh=325&amp;ima=fit&amp;impolicy=Letterbox&amp;imcolor=%23000000&amp;letterbox=true')
     if rand == 3:
         bot.send_photo(chat_id=message.chat.id, caption='♿Аирлейнс♿', photo='https://cs.pikabu.ru/post_img/2013-01_2/1357669124_46598435.jpg')
-    datas = {
-
-    }
 
 bot.polling(none_stop=True)
